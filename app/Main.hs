f29b007c3dadcbf50dbf816b80cfd3fb1842d17f
Benjamin <benjamin.guy.thomas@gmail.com>
2022-06-02 19:53:49 +0200

    Use postgresql-typed!

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 {-
 
 Tool chain installation:
@@ -7,8 +11,6 @@ Tool chain installation:
 Run program:
 
     echo app/Main.hs | entr -c cabal run pg-haskell
-    echo app/Main.hs | entr -c runghc ./app/Main.hs
-    echo app/Main.hs | entr -c runhaskell ./app/Main.hs
 
 Build program:
 
@@ -38,74 +40,47 @@ References:
     - https://lotz84.github.io/haskellbyexample
 
 -}
-{-# LANGUAGE OverloadedStrings #-}
+module Main (
+    main,
+) where
 
-module Main (main) where
+import Control.Monad (
+    unless,
+    void,
+ )
+import DBConfig (dbConn)
+import Data.Int (Int32)
+import Data.Maybe (listToMaybe)
+import Database.PostgreSQL.Typed as PG (
+    PGConnection,
+    pgConnect,
+    pgExecute,
+    pgQuery,
+    pgSQL,
+ )
 
--- Following this tutorial: https://tuttlem.github.io/2020/10/30/postgresql-data-access-with-haskell.html
+data Client = Client Int32 String
+    deriving (Eq)
 
-import Data.Int (Int64)
-import Database.PostgreSQL.Simple
-  ( ConnectInfo
-      ( connectDatabase,
-        connectHost,
-        connectPassword,
-        connectUser
-      ),
-    Connection,
-    FromRow,
-    Only (Only),
-    connect,
-    defaultConnectInfo,
-    query,
-  )
-import Database.PostgreSQL.Simple.FromRow (FromRow (..), field)
-import GHC.GHCi.Helpers (flushAll)
+insertClient :: PGConnection -> Client -> IO ()
+insertClient pg (Client cid name) =
+    void $
+        pgExecute
+            pg
+            [pgSQL|INSERT INTO clients (id, name) VALUES (${cid}, ${name})|]
 
--- import System.Environment
-
--- x :: String
--- x = "YOZ"
-
--- home :: IO String
--- home = getEnv "HOME"
-
--- mHome :: IO (Maybe String)
--- mHome = lookupEnv "HOME"
-
-localPG :: ConnectInfo
-localPG =
-  defaultConnectInfo
-    { connectHost = "SHOULD_LOAD_FROM_ENV",
-      connectDatabase = "SHOULD_LOAD_FROM_ENV",
-      connectUser = "SHOULD_LOAD_FROM_ENV",
-      connectPassword = "SHOULD_LOAD_FROM_ENV"
-    }
-
-data Client = Client {id :: Int, name :: String}
-  deriving (Show)
-
-instance FromRow Client where
-  fromRow = Client <$> field <*> field
-
-selectClient :: Connection -> Int -> IO [Client]
-selectClient conn cid = query conn "SELECT id, name FROM clients WHERE id = ?" $ Only cid
-
-insertClient :: Connection -> String -> IO [Only Int64]
-insertClient conn name_ =
-  query conn "INSERT INTO clients (name) VALUES (?) RETURNING id" $ Only name_
+getClient :: PGConnection -> Int32 -> IO (Maybe Client)
+getClient pg cid =
+    fmap (uncurry Client) . listToMaybe
+        <$> pgQuery
+            pg
+            [pgSQL|SELECT id, name FROM clients WHERE id = ${cid}|]
 
 main :: IO ()
 main = do
-  conn <- connect localPG
-  putStr "What is the client's name? "
-  flushAll
-  clientName <- getLine
-  cid <- insertClient conn clientName
-  putStrLn $ "New Client: " ++ show cid
-  putStrLn "Now retrieving client #1031600975 from test database..."
-  client <- selectClient conn 1031600975
-  putStrLn $ "Client #1031600975 details: " ++ show client
-  putStrLn "Finished!"
-
---   mapM_ print =<< (query_ conn "SELECT 1 + 2" :: IO [Only Int])
+    db <- pgConnect =<< dbConn
+    let john = Client 1 "John Doe"
+    insertClient db john
+    johnAgain <- getClient db 1
+    unless (johnAgain == Just john) $ fail "Clients don't match!"
+    putStrLn "DONE!"
